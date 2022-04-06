823bf97295b626f0f01839a0325ec20de5354d3e
Will Choe <jazzbearz@gmail.com>
2022-04-06 16:52:15 +0900

    thread_local intialization check

--- src_c/th.cc
+++ src_c/th.cc
@@ -9,29 +9,37 @@
 static std::atomic<uint32_t> TINC(0);
 static std::set<uint32_t> TSET;
 static std::mutex T_MUTEX;
+thread_local bool is_initialized = false;
 thread_local uint32_t TNUM = 0;
 thread_local void *TPTR_ARRAY[TLOCAL_ARRAY_LEN];
 
 void initialize_thread_number()
 {
+    if (!is_initialized)
     {
-        std::lock_guard<std::mutex> guard(T_MUTEX);
-        if (TSET.size() > 0)
         {
-            auto begin = TSET.begin();
-            TNUM = *begin;
-            TSET.erase(TNUM);
-            return;
+            std::lock_guard<std::mutex> guard(T_MUTEX);
+            if (TSET.size() > 0)
+            {
+                auto begin = TSET.begin();
+                TNUM = *begin;
+                TSET.erase(TNUM);
+                return;
+            }
         }
+        auto tnum = TINC.fetch_add(1, std::memory_order::memory_order_relaxed);
+        TNUM = tnum;
+        is_initialized = true;
     }
-    auto tnum = TINC.fetch_add(1, std::memory_order::memory_order_relaxed);
-    TNUM = tnum;
 }
 
 void uninitialize_thread_number()
 {
-    std::lock_guard<std::mutex> guard(T_MUTEX);
-    TSET.insert(TNUM);
+    if (is_initialized)
+    {
+        std::lock_guard<std::mutex> guard(T_MUTEX);
+        TSET.insert(TNUM);
+    }
 }
 
 unsigned int *get_thread_number()
